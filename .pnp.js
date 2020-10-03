1bfd55192aef48115d1172e5ef789567f849adf3
CircleCI <devops@sprucelabs.ai>
2020-10-03 20:56:54 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- .pnp.js
+++ .pnp.js
@@ -39,7 +39,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageDependencies": [
             ["@semantic-release/changelog", "virtual:8691a3a3b981d9951d6a1b39d599f07c4479304974097cf39a78430b18ebeb98d86d8e51887cb405f1b72302f1426ba758fd98908c25c1fd1d03cd0f7fa21ab2#npm:5.0.1"],
             ["@semantic-release/git", "virtual:8691a3a3b981d9951d6a1b39d599f07c4479304974097cf39a78430b18ebeb98d86d8e51887cb405f1b72302f1426ba758fd98908c25c1fd1d03cd0f7fa21ab2#npm:9.0.0"],
-            ["@sprucelabs/error", "npm:2.2.400"],
+            ["@sprucelabs/error", "npm:2.2.401"],
             ["@sprucelabs/semantic-release", "npm:4.0.5"],
             ["@sprucelabs/test", "npm:7.0.95"],
             ["@types/lodash", "npm:4.14.161"],
@@ -2356,10 +2356,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]
       ]],
       ["@sprucelabs/error", [
-        ["npm:2.2.400", {
-          "packageLocation": "./.yarn/cache/@sprucelabs-error-npm-2.2.400-a28fdaa5b1-31149a3885.zip/node_modules/@sprucelabs/error/",
+        ["npm:2.2.401", {
+          "packageLocation": "./.yarn/cache/@sprucelabs-error-npm-2.2.401-659f428321-cb35e913ff.zip/node_modules/@sprucelabs/error/",
           "packageDependencies": [
-            ["@sprucelabs/error", "npm:2.2.400"]
+            ["@sprucelabs/error", "npm:2.2.401"]
           ],
           "linkType": "HARD",
         }]
@@ -2404,7 +2404,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@sprucelabs/test-utils", "workspace:."],
             ["@semantic-release/changelog", "virtual:8691a3a3b981d9951d6a1b39d599f07c4479304974097cf39a78430b18ebeb98d86d8e51887cb405f1b72302f1426ba758fd98908c25c1fd1d03cd0f7fa21ab2#npm:5.0.1"],
             ["@semantic-release/git", "virtual:8691a3a3b981d9951d6a1b39d599f07c4479304974097cf39a78430b18ebeb98d86d8e51887cb405f1b72302f1426ba758fd98908c25c1fd1d03cd0f7fa21ab2#npm:9.0.0"],
-            ["@sprucelabs/error", "npm:2.2.400"],
+            ["@sprucelabs/error", "npm:2.2.401"],
             ["@sprucelabs/semantic-release", "npm:4.0.5"],
             ["@sprucelabs/test", "npm:7.0.95"],
             ["@types/lodash", "npm:4.14.161"],
