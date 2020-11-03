b673adc7773ef179b99f75b2a06685aa6c6848e5
CircleCI <devops@sprucelabs.ai>
2020-11-03 03:56:50 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- .pnp.js
+++ .pnp.js
@@ -3661,7 +3661,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageDependencies": [
             ["browserslist", "npm:4.14.6"],
             ["caniuse-lite", "npm:1.0.30001154"],
-            ["electron-to-chromium", "npm:1.3.585"],
+            ["electron-to-chromium", "npm:1.3.586"],
             ["escalade", "npm:3.1.1"],
             ["node-releases", "npm:1.1.65"]
           ],
@@ -5026,10 +5026,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]
       ]],
       ["electron-to-chromium", [
-        ["npm:1.3.585", {
-          "packageLocation": "./.yarn/cache/electron-to-chromium-npm-1.3.585-557e65f54f-a7cd851563.zip/node_modules/electron-to-chromium/",
+        ["npm:1.3.586", {
+          "packageLocation": "./.yarn/cache/electron-to-chromium-npm-1.3.586-e0ddd2d533-3c680e665a.zip/node_modules/electron-to-chromium/",
           "packageDependencies": [
-            ["electron-to-chromium", "npm:1.3.585"]
+            ["electron-to-chromium", "npm:1.3.586"]
           ],
           "linkType": "HARD",
         }]
