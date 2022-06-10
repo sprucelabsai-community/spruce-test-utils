61625341fc57ab8bae9bb0c27b40f64b1c4c9b63
Ryan Johnson <ryan@sprucelabs.ai>
2022-06-10 08:13:23 -0400

    minor: Now can generateId without outside dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as errorAssert } from './utilities/errorAssert'
 export { default as errorAssertUtil } from './utilities/errorAssert.utility'
+export { default as generateId } from './utilities/generateId.utility'
---
+++ src/utilities/generateId.utility.ts
@@ -0,0 +1,5 @@
+import crypto from 'crypto'
+
+export default function generateId() {
+	return crypto.randomUUID().replace(/[^a-z0-9]/gi, '')
+}
