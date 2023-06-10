f6b0d65f2ffa4665e2067666af39273c64a2f780
Taylor Romero <me@taylorrome.ro>
2023-06-10 10:00:41 -0600

    patch: exprot recursive partial type

--- src/assert/assert.ts
+++ src/assert/assert.ts
@@ -8,7 +8,7 @@ import assertUtil from './assert.utility'
 
 const stringify = assertUtil.stringify.bind(assertUtil)
 
-type RecursivePartial<T> = {
+export type RecursivePartial<T> = {
 	[P in keyof T]?: T[P] extends (infer U)[]
 		? RecursivePartial<U>[] // eslint-disable-next-line @typescript-eslint/ban-types
 		: T[P] extends object
