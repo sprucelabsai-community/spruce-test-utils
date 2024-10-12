628f01c9eeb273c23a5e1dd7983629ceca4dd35e
Taylor Romero <me@taylorrome.ro>
2024-10-12 08:02:26 -0600

    patch: stack cleaner does not clean message

--- src/AssertionError.ts
+++ src/AssertionError.ts
@@ -4,8 +4,10 @@ export default class AssertionError extends Error {
     public constructor(message: string, stack?: string) {
         super(message)
         this.message = message
-        this.stack = StackCleaner.clean(
-            `${this.message}${(stack ?? this.stack ?? '').replace(message, '')}`
-        )
+        this.stack =
+            this.message +
+            StackCleaner.clean(
+                `${(stack ?? this.stack ?? '').replace(message, '')}`
+            )
     }
 }
