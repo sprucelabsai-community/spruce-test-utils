8ad8743f35d297b585fb374ddaa50887709682f7
Taylor Romero <me@taylorrome.ro>
2025-02-22 12:18:10 -0700

    patch: few extra tests

--- src/__tests__/behavioral/StaticParentTestCanAccessParentMethods.test.ts
+++ src/__tests__/behavioral/StaticParentTestCanAccessParentMethods.test.ts
@@ -3,11 +3,29 @@ import test from '../../decorators'
 import AbstractStaticTest from './support/AbstractStaticTest'
 
 export default class StaticParentTestCanAccessParentMethodsTest extends AbstractStaticTest {
-    @test()
-    protected static async didCallBeforeAllScopedCorrectly() {
+    protected static didCallFirstTest = false
+
+    protected static async afterAll(): Promise<void> {
+        await super.afterAll()
         assert.isTrue(
             this.didCallAnotherStaticMethodInBeforeEach,
             'beforeAll in parent class not referrenced'
         )
+
+        assert.isTrue(this.didCallFirstTest, 'First test was not called')
+    }
+
+    @test()
+    protected static async didCallBeforeAllScopedCorrectly() {
+        this.didCallFirstTest = true
+        assert.isTrue(
+            this.didCallAnotherStaticMethodInBeforeEach,
+            'beforeAll in parent class not referrenced'
+        )
+    }
+
+    @test()
+    protected static async shouldCallFirstTest() {
+        assert.isTrue(this.didCallFirstTest, 'First test was not called')
     }
 }
